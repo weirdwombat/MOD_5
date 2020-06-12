f8352a6bb543c927957fcae3a135cdc46a018b00
weirdwombat <omkennedy@dmacc.edu>
2020-06-12 06:50:37 -0500

    Basic For Loops

---
+++ basic_for_loops.py
@@ -0,0 +1,19 @@
+"""
+Program : basic_for_loops.py
+Author : Olivia Kennedy
+Date Last Modified : 06/11/2020
+This program will print a short list of floating point numbers and the odd numbers
+from 99 to 33 in descending order using for loops to print both.
+"""
+print("A Short List of Floating Point Numbers")
+
+floating_point_numbers = [0.1 , 0.2, 0.3]
+for f in floating_point_numbers:
+    print(f)
+
+print()
+
+print("The Odd Numbers From 99 to 33 in Descending Order")
+for z in range (99, 31, -2):
+    print(z)
+
