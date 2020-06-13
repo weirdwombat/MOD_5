26aff0675f3ce7be4c33b9cbb7665741e37932f0
weirdwombat <omkennedy@dmacc.edu>
2020-06-13 11:29:54 -0500

    input_while_exit.py

--- input_while_exit.py
+++ input_while_exit.py
@@ -4,13 +4,12 @@ user_input = int(input("Enter any number from 1 to 100, otherwise, enter -99 to 
 
 
 while user_input != -99:
-    while 1 <= user_input or 100 >= user_input:
+    if 1 <= user_input and 100 >= user_input:
         user_list.append(user_input)
         user_input = int(input("Enter any number from 1 to 100, otherwise, enter -99 to view the list of good input:"))
         if 1 > user_input or 100 < user_input:
             break
-    if 1 > user_input or 100 < user_input:
-        break
+    user_input = int(input("Enter any number from 1 to 100, otherwise, enter -99 to view the list of good input:"))
 
 #for loop
 for u in user_list:
